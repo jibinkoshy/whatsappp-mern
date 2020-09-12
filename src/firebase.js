bd1937184a6c1cd49629386ad4eea0682b3dd6eb
jibinkoshy <jibinkoshy@hotmail.com>
2020-09-12 15:40:12 -0500

    changes

--- src/firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: 'AIzaSyC40441CI6JDc8EQweSaw0lpAxVA8vb1Xw',
-  authDomain: 'whatsapp-mern-a2a14.firebaseapp.com',
-  databaseURL: 'https://whatsapp-mern-a2a14.firebaseio.com',
-  projectId: 'whatsapp-mern-a2a14',
-  storageBucket: 'whatsapp-mern-a2a14.appspot.com',
-  messagingSenderId: '953278609339',
-  appId: '1:953278609339:web:49d468d5897d65552f369e',
-};
