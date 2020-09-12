1cab52f75f434286c58197ef42438fbc48c77197
jibinkoshy <jibinkoshy@hotmail.com>
2020-09-12 15:50:29 -0500

    add sidebar and chat component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
+import Chat from './Chat';
+import Sidebar from './Sidebar';
 
 function App() {
-  return <div className="app">Hello World</div>;
+  return (
+    <div className="app">
+      <Sidebar />
+      <Chat />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Sidebar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Sidebar.css';
+
+const Sidebar = () => {
+  return <div className="sidebar">Im a Sidebar</div>;
+};
+
+export default Sidebar;
