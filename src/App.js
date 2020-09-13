350858875dca26d9f37fe02f0eea2b5a189072ac
jibinkoshy <jibinkoshy@hotmail.com>
2020-09-13 13:52:32 -0500

    Add pusher-js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Pusher from 'pusher-js';
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
 
 function App() {
+  useEffect(() => {
+    const pusher = new Pusher('7c784c2f344754b24efc', {
+      cluster: 'us2',
+    });
+
+    var channel = pusher.subscribe('messages');
+    channel.bind('inserted', function (data) {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
   return (
     <div className="app">
       <div className="app__body">
