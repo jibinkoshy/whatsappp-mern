8a0e998d96ff6ab627af1a9c8e8fc32a2e9ca18c
jibinkoshy <jibinkoshy@hotmail.com>
2020-09-13 15:22:22 -0500

    call backend with axios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Pusher from 'pusher-js';
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
+import axios from './axios';
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    async function fetchMessage() {
+      const res = await axios.get('/messages/sync');
+      // console.log('res:::', res.data);
+      const messages = await res.data;
+      setMessages(messages);
+    }
+    fetchMessage();
+  }, []);
   useEffect(() => {
     const pusher = new Pusher('7c784c2f344754b24efc', {
       cluster: 'us2',
     });
 
     var channel = pusher.subscribe('messages');
-    channel.bind('inserted', function (data) {
-      alert(JSON.stringify(data));
+    channel.bind('inserted', function (newMessage) {
+      // alert(JSON.stringify(newMessage));
+
+      setMessages((messages) => [...messages, newMessage]);
     });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
   }, []);
+
+  console.log('messages:::', messages);
 
   return (
     <div className="app">
       <div className="app__body">
         <Sidebar />
-        <Chat />
+        <Chat messages={messages} />
       </div>
     </div>
   );
--- src/Chat.js
+++ src/Chat.js
@@ -1,5 +1,6 @@
+import React, { useState } from 'react';
 import { Avatar, IconButton } from '@material-ui/core';
-import React from 'react';
+import axios from './axios';
 import './Chat.css';
 
 import {
@@ -9,7 +10,22 @@ import {
   MoreVert,
   SearchOutlined,
 } from '@material-ui/icons';
-const Chat = () => {
+const Chat = ({ messages }) => {
+  const [input, setInput] = useState('');
+
+  const sendMessage = async (e) => {
+    console.log('input:::', input);
+    e.preventDefault();
+    await axios.post('/messages/new', {
+      message: input,
+      name: 'Jibin Koshy',
+      timestamp: 'Just now!',
+      received: true,
+    });
+
+    setInput('');
+  };
+
   return (
     <div className="chat">
       <div className="chat__header">
@@ -34,7 +50,17 @@ const Chat = () => {
       </div>
 
       <div className="chat__body">
-        <p className="chat__message">
+        {messages.map((message) => (
+          <p
+            className={`chat__message ${message.received && 'chat__receiver'}`}
+          >
+            <span className="chat__name">{message.name}</span>
+            {message.message}
+            <span className="chat__timestamp">{message.timestamp}</span>
+          </p>
+        ))}
+
+        {/* <p className="chat__message">
           <span className="chat__name">Jibin</span>
           This is a message
           <span className="chat__timestamp">{new Date().toUTCString()}</span>
@@ -50,22 +76,19 @@ const Chat = () => {
           <span className="chat__name">Jibin</span>
           This is a message
           <span className="chat__timestamp">{new Date().toUTCString()}</span>
-        </p>
+        </p> */}
       </div>
 
       <div className="chat__footer">
         <InsertEmoticon />
         <form>
           <input
-            // value={input}
-            // onChange={(e) => setInput(e.target.value)}
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
             placeholder="Type a message"
             type="text"
           />
-          <button
-            //    onClick={sendMessage}
-            type="submit"
-          >
+          <button onClick={sendMessage} type="submit">
             Send a message
           </button>
         </form>
