1631f87b8ada3d1ff034b253349409cc8542cf8e
jibinkoshy <jibinkoshy@hotmail.com>
2020-09-12 19:28:25 -0500

    add chat body and chat footer

--- src/Chat.js
+++ src/Chat.js
@@ -2,7 +2,13 @@ import { Avatar, IconButton } from '@material-ui/core';
 import React from 'react';
 import './Chat.css';
 
-import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
+import {
+  AttachFile,
+  InsertEmoticon,
+  Mic,
+  MoreVert,
+  SearchOutlined,
+} from '@material-ui/icons';
 const Chat = () => {
   return (
     <div className="chat">
@@ -25,6 +31,45 @@ const Chat = () => {
             <MoreVert />
           </IconButton>
         </div>
+      </div>
+
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Jibin</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat__message chat__receiver">
+          <span className="chat__name">Jibin</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat__message">
+          <span className="chat__name">Jibin</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+
+      <div className="chat__footer">
+        <InsertEmoticon />
+        <form>
+          <input
+            // value={input}
+            // onChange={(e) => setInput(e.target.value)}
+            placeholder="Type a message"
+            type="text"
+          />
+          <button
+            //    onClick={sendMessage}
+            type="submit"
+          >
+            Send a message
+          </button>
+        </form>
+        <Mic />
       </div>
     </div>
   );
